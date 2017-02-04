4424bb02f6b40d13156cd63cebf1d1ba4d7fd267
Debayan Kar <kardebayan3@gmail.com>
2017-02-04 14:16:03 +0000

    atu-l31: Add kernel flags
    Change-Id: If241f2d15aadad5dd8020593018f64f7ec7e55d1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,3 +23,18 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msm8917
 TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/huawei/atu_l31
+TARGET_KERNEL_CONFIG := atu_defconfig
