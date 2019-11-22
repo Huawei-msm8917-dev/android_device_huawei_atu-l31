b9ff2ec20823e8a547f77b3f77d9b371d4628276
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    atu-l31: Switch to Audio HAL V6
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test on taimen and walleye
    
    Change-Id: Ifcd79be517daf1ba42459b07a492869a58efc4fe

--- device.mk
+++ device.mk
@@ -6,9 +6,9 @@
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_PACKAGES += \
