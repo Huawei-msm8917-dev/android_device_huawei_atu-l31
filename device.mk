7a93be8f8987a16d6a4e90567f2d7f90b1ceb8ab
Debayan Kar <kardebayan3@gmail.com>
2021-10-27 16:40:39 +0530

    atu-l31: Import IRQ config from stock
    * From Huawei/generic_a53/generic_a53:8.0.0/OPR6.170623.013/jenkin07042216:user/test-keys
    
    Change-Id: Ie50dcd2439673857a6c0ee4dc2f065eca2f204fa
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- device.mk
+++ device.mk
@@ -54,6 +54,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
+# IRQ
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/irq/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
