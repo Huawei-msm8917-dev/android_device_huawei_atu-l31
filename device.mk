af46dbd2ffc342a7de08b67fa4d8fd31b45bab73
Debayan Kar <kardebayan3@gmail.com>
2021-10-29 22:26:41 +0530

    atu-l31: Build Thermal HIDLs from source
    Change-Id: I4d972dc1d905313987df013d218d6502a9b7ca45
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- device.mk
+++ device.mk
@@ -178,6 +178,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
