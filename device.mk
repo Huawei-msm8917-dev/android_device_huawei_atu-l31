be00efb7f1586f1bee0355d024e860744543d384
Debayan Kar <kardebayan3@gmail.com>
2021-10-30 15:07:43 +0530

    atu-l31: rootdir: Update from LA.UM.8.6.2.c26-01200-89xx.0
    Change-Id: I74ef41a8eea28823767df6dc487e40057405786c

--- device.mk
+++ device.mk
@@ -144,27 +144,14 @@ PRODUCT_COPY_FILES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    init.class_main.sh \
-    init.crda.sh \
-    init.mdm.sh \
-    init.qcom.class_core.sh \
-    init.qcom.coex.sh \
     init.qcom.early_boot.sh \
-    init.qcom.efs.sync.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sdio.sh \
-    init.qcom.sensors.sh \
     init.qcom.sh \
-    init.qcom.syspart_fixup.sh \
-    init.qcom.usb.sh \
-    init.qcom.wifi.sh \
-    init.qti.fm.sh \
-    init.qti.ims.sh \
-    qca6234-service.sh \
     init.msm.usb.configfs.rc \
     init.qcom.factory.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
+    init.recovery.qcom.rc \
     init.target.rc \
     fstab.qcom \
     ueventd.rc
