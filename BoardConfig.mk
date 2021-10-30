0bd2c447ac4d5aae8975d6533d2d16ba8694f94a
Debayan Kar <kardebayan3@gmail.com>
2021-10-31 01:09:59 +0530

    atu-l31: Import config.fs from LA.UM.8.6.2.c26-01200-89xx.0
    Change-Id: I667c593aaf40402ebe3061586bdad58082556265

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,9 @@ TARGET_USES_GRALLOC1 := true
 TARGET_USES_HWC2 := true
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
